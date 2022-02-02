7a3cf438647966be713ac10d874ed8897f582eb9
crystal cabins <crystalcabinss@gmail.com>
2022-02-02 10:52:40 +0100

    added better comments 📃

--- src/Polygon.java
+++ src/Polygon.java
@@ -15,24 +15,42 @@ public class Polygon {
     lengthSides = new double[numberSides];
   }
 
+  /**
+   * @param _numberSides number of sides of the polygon you want to create
+   */
   Polygon(int ns) {
     numberSides = ns;
     lengthSides = new double[ns];
   }
 
+  /**
+   * @param _numberSides number of sides of the polygon you want to create
+   * @param _lengthSides the length array for all the sides of your polygon
+   */
   Polygon(int ns, double[] ls) {
     numberSides = ns;
     lengthSides = ls;
   }
 
+  /**
+   * @param lengthSides Set the length array of the polygon to this value
+   */
   public void setLengthSides(double[] lengthSides) {
     this.lengthSides = lengthSides;
   }
 
+  /**
+   * @return the length array of the polygon
+   */
   public double[] getLengthSides() {
     return lengthSides;
   }
 
+  /**
+   * Calculates the perimeter of the polygon.
+   * 
+   * @return perimeter of the polygon
+   */
   public double getPerimeter() {
     double perimeter = 0;
     for (int i = 0; i < lengthSides.length; i++) {
@@ -41,6 +59,11 @@ public class Polygon {
     return perimeter;
   }
 
+  /**
+   * Return the string representation of the polygon.
+   * 
+   * @return string representation of the polygon
+   */
   public String toString() {
     String result = "Sides: ";
     for (int i = 0; i < lengthSides.length; i++) {
